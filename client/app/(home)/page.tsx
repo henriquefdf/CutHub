dc2424fc4e91bb1ddb19f58cd9bb4b99c82d593b
marcelomrad <marcelomrad@gmail.com>
2024-03-18 19:17:18 -0300

    feat: componente header

--- client/app/(home)/page.tsx
+++ client/app/(home)/page.tsx
@@ -1,21 +1,32 @@
 'use client';
 
+import Header from "../_components/header";
+
+import { format } from "date-fns";
+import { ptBR } from "date-fns/locale";
 
 import { useContext } from "react";
 import { AuthContext } from "../_contexts/AuthContext";
 
 
-import DadosUsuario from './dados-usuario';
-
 function Home() {
     const { user } = useContext(AuthContext);
-  return (
-    <div>
-      <h1>Home</h1>
-      <DadosUsuario user={user} />
-    </div>
-  );
+    return (
+        <div>
+            <Header />
+            <div className="px-5 pt-5">
+                <h2 className="text-xl font-bold">
+                    {user ? `Olá, ${user.nome.split(" ")[0]}!` : "Olá! Vamos agendar um corte hoje?"}
+                </h2>
+                <p className="capitalize text-sm">
+                    {format(new Date(), "EEEE',' dd 'de' MMMM", {
+                        locale: ptBR,
+                    })}
+                </p>
+            </div>
+        </div>
+    );
 }
- 
+
 export default Home;
 
---
+++ client/app/_components/header.tsx
@@ -0,0 +1,38 @@
+"use client";
+
+import { MenuIcon } from 'lucide-react';
+import { Button } from './ui/button';
+import { Card, CardContent } from './ui/card';
+import Image from 'next/image';
+import { Sheet, SheetContent, SheetTrigger } from './ui/sheet';
+// import SideMenu from './side-menu';
+import Link from 'next/link';
+
+
+const Header = () => {
+
+
+    return (
+        <Card>
+            <CardContent className='p-5 justify-between items-center flex flex-row'>
+                <Link href="/">
+                    <Image src="/logo.png" alt="Logo" width={82} height={22} />
+                </Link>
+
+                <Sheet>
+                    <SheetTrigger asChild>
+                        <Button variant="outline" size="icon">
+                            <MenuIcon size={18} />
+                        </Button>
+                    </SheetTrigger>
+
+                    <SheetContent className="p-0">
+                        {/* <SideMenu /> */}
+                    </SheetContent>
+                </Sheet>
+            </CardContent>
+        </Card>
+    );
+}
+
+export default Header;
