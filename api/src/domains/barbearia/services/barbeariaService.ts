475c4e2fc2b8273a4314611af9840e1bda1981c4
arthurlm1234 <arthurlm1234@gmail.com>
2024-03-22 08:18:20 -0300

    Adiciona importação de função deleteObject e atualiza lógica de atualização de foto da barbearia

--- api/src/domains/barbearia/services/barbeariaService.ts
+++ api/src/domains/barbearia/services/barbeariaService.ts
@@ -1,6 +1,7 @@
 import prisma from "../../../../config/prismaClient";
 import { NotAuthorizedError } from "../../../../errors/NotAuthorizedError";
 import { Barbearia } from "@prisma/client";
+import { deleteObject } from "../../../../utils/functions/aws";
 
 export class barbeariaService {
     
@@ -10,6 +11,7 @@ export class barbeariaService {
               nome: body.nome,
               endereco: body.endereco,
               foto: (file as Express.MulterS3.File).location,
+              chaveAws: (file as Express.MulterS3.File).key,
               usuarioId: idUsuario,
             },
           });
@@ -22,8 +24,20 @@ export class barbeariaService {
           where: { usuarioId: +idUsuario }
         });
 
+        console.log(barbearia);
+
         if(barbearia?.usuarioId !== idUsuario) {
             throw new NotAuthorizedError('Usuário não autorizado.');
+        }
+
+        if(file){
+          deleteObject(barbearia?.chaveAws!);
+          body.foto = (file as Express.MulterS3.File).location;
+          body.chaveAws = (file as Express.MulterS3.File).key;
+        }
+        else{
+          body.foto = barbearia?.foto;
+          body.chaveAws = barbearia?.chaveAws;
         }
 
         const barbeariaAtualizada = await prisma.barbearia.update({
@@ -31,7 +45,8 @@ export class barbeariaService {
             data: {
                 nome: body.nome,
                 endereco: body.endereco,
-                foto: (file as Express.MulterS3.File).location,
+                foto: body.foto,
+                chaveAws: body.chaveAws,
             }
         });
 
