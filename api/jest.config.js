1f4df86316f3dd332c05217524f788b8a1cc722c
arthurlm1234 <arthurlm1234@gmail.com>
2024-03-22 09:44:28 -0300

    Adiciona configuração do Jest para cobertura de teste

---
+++ api/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**/*Service.ts'],
+};
